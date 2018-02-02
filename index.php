a3eeebe4be08ceb4b44d37d39d1dd418c1a283f8
Charlie Gao <chg774@g.harvard.edu>
2018-02-01 20:44:23 -0500

    add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Hello World</title>
+	<meta charset='utf-8'>
+		
+</head>
+<body>
+
+	<?php 
+	echo 'Hello World! You are visiting ';
+	echo $_SERVER['SERVER_NAME']; 
+	?>
+	
+</body>
+</html>
